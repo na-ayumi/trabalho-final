be80caac36fd1df1b57a104a7663f277128f2632
Natalia Ayumi Shimizu da Silva <nshimizudasilva@gmail.com>
2026-01-27 20:39:27 -0400

    Atualização - Repositório Prisma

---
+++ src/infra/database/prisma/PrismaRentalRepository.ts
@@ -0,0 +1,25 @@
+import { injectable } from "inversify";
+import { prisma } from "./client";
+import { Rental } from "../../../domain/entities/Rental";
+import { IRentalRepository } from "../../../domain/repositories/IRentalRepository";
+
+@injectable()
+export class PrismaRentalRepository implements IRentalRepository{
+    async findById(id: string): Promise<Rental | null> {
+        const rental = await prisma.rental.findUnique({
+            where: {id}
+        })
+
+        return rental;
+    }
+
+    async createRental(rental: Rental): Promise<void> {
+        await prisma.rental.create({
+            id: rental.id,
+            carId: rental.carId,
+            startDate: rental.startDate,
+            endDate: rental.endDate,
+            createAt: rental.createAt
+        })
+    }
+}
