52479d23ad5215b81168869e1c29b1eea522eb45
Natalia Ayumi Shimizu da Silva <nshimizudasilva@gmail.com>
2026-02-02 19:37:10 -0400

    Correção - CLI

--- src/adapters/cli/main.ts
+++ src/adapters/cli/main.ts
@@ -1,59 +1,21 @@
-import { Command } from 'commander';
 import 'reflect-metadata';
 import { contanier } from '../../infra/container/index.js'
 import { CreateRentalUseCase } from '../../application/useCases/createRental/CreateRentalUseCase.js';
 
-const program = new Command();
+async function main() {
+  const createRentalUseCase = contanier.get(CreateRentalUseCase);
 
-program
-  .name('locacao-cli')
-  .description('CLI para locação de veículos')
-  .version('1.0.0');
-
-program
-  .command('criar-aluguel')
-  .description('Cria um novo aluguel')
-  .requiredOption('--id <string>', 'ID do aluguel')
-  .requiredOption('--carId <string>', 'ID do carro')
-  .requiredOption('--start <string>', 'Data início')
-  .requiredOption('--end <string>', 'Data fim')
-  .action(async (options) => {
-    try {
-      const createRentalUseCase =
-        contanier.get(CreateRentalUseCase);
-
-      await createRentalUseCase.execute({
-        id: options.id,
-        carId: options.carId,
-        startDate: new Date(options.start),
-        endDate: new Date(options.end),
-        createAt: new Date()
-      });
-
-      console.log('Aluguel criado com sucesso!');
-    } catch (error: any) {
-      console.error('Erro:', error.message);
-    }
+  await createRentalUseCase.execute({
+    id: 'rental-simulado',
+    licensePlate: 'HFP-3762',
+    startDate: new Date('2026-03-02T10:00:00'),
+    endDate: new Date ('2026-03-05T13:00:000'),
+    createAt: new Date()
   });
 
-program.parse(process.argv);
+  console.log('O objeto Rental foi criado.');
+}
 
-// para rodar:
-// npm run dev -- criar-aluguel \
-//   --id r1 \
-//   --carId c1 \
-//   --start 2024-11-01T10:00:00 \
-//   --end 2024-11-02T12:00:00
-
-// Para ver no Banco de Dados:
-// npx prisma studio
-
-// para rodar os testes (instalar):
-//npm install commander
-//npm install -D tsx
-// --npm i --save-dev @types/node
-// --npm install inversify reflect-metadata
-// --npm install -D vitest
-// --npm install -D supertest @types/supertest
-// rodar no terminal:
-// npx vitest CreateRentalUseCase.spec.ts
+main().catch(error => {
+  console.error('Erro ao executar o CLI: ', error.message);
+});
